8d0513ca19d00e78d89f8b3eb7ea95d4b226313d
yavordobrev24 <yavordobrev24@gmail.com>
2024-03-07 23:15:28 +0200

    Dynamic movies and tv-shows lists ready, data seeded, connected to REST API

--- frontend/src/app/movies-list/movies-list.component.ts
+++ frontend/src/app/movies-list/movies-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
-  styleUrls: ['./movies-list.component.css']
+  styleUrls: ['./movies-list.component.css'],
 })
-export class MoviesListComponent {
-
+export class MoviesListComponent implements OnInit {
+  movies: any = [];
+  constructor(private apiService: ApiService) {}
+  ngOnInit(): void {
+    this.movies = this.apiService.getMovies();
+  }
+  details(e: any) {
+    console.log(e.currentTarget);
+  }
 }
--- frontend/src/app/tv-shows-list/tv-shows-list.component.ts
+++ frontend/src/app/tv-shows-list/tv-shows-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-tv-shows-list',
   templateUrl: './tv-shows-list.component.html',
-  styleUrls: ['./tv-shows-list.component.css']
+  styleUrls: ['./tv-shows-list.component.css'],
 })
-export class TvShowsListComponent {
-
+export class TvShowsListComponent implements OnInit {
+  tvShows: any = [];
+  constructor(private apiService: ApiService) {}
+  ngOnInit(): void {
+    this.tvShows = this.apiService.getTVShows();
+  }
+  details(e: any) {
+    console.log(e.currentTarget);
+  }
 }
