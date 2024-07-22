9fb9199c115a0f1f568fde36b8799342d8e2d6bf
Yavor Dobrev <yavordobrev24@gmail.com>
2024-07-22 14:38:55 +0300

    Enable cookies and configure CORS for the Express application

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,39 +1,28 @@
 import express, { Request, Response } from 'express'
 import { AppDataSource } from './data-source'
+import cookieParser from 'cookie-parser'
 import 'dotenv/config'
-import { cacheMiddleware } from './middlewares/cacheMiddleware'
+import cors from 'cors'
 
-const { PORT, TMDB_API_KEY, TMDB_API_URL } = process.env
+
+const { PORT } = process.env
 const app = express()
 
 AppDataSource.initialize()
   .then(() => {
     console.log('Data Source has been initialized!')
   })
-  .catch((err) => {
-    console.error('Error during Data Source initialization:', err)
+  .catch((error) => {
+    console.error('Error during Data Source initialization:', error)
   })
-
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}
+app.use(cors(corsOptions))
 app.use(express.json())
-
-app.get('/api/movies', cacheMiddleware, async (req: Request, res: Response) => {
-  console.log('Full Request!')
-
-  try {
-    const response = await fetch(`${TMDB_API_URL}/3/discover/movie`, {
-      headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${TMDB_API_KEY}`,
-      },
-    })
-
-    const data = await response.json()
-    console.log(data)
-    res.json(data)
-  } catch (err) {
-    console.log(err)
-    res.status(500).send('Internal Server Error')
-  }
-})
+app.use(cookieParser())
 
 app.listen(PORT || 3000)
