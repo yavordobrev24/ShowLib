3a182214e58996a9d7e6a92e44788f317b387ec2
Yavor Dobrev <yavordobrev24@gmail.com>
2024-07-22 14:55:05 +0300

    Create the User entity and include it in the data source configuration

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 
 import 'dotenv/config'
+import { User } from './entity/User'
 const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env
 
 export const AppDataSource = new DataSource({
@@ -13,7 +14,7 @@ export const AppDataSource = new DataSource({
   database: DB_NAME,
   synchronize: true,
   logging: false,
-  entities: [],
+  entities: [User],
   migrations: [],
   subscribers: [],
 })
