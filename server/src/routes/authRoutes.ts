0b0d9466d88b53b803cded16eba865fc63684208
Yavor Dobrev <yavordobrev24@gmail.com>
2024-08-10 10:23:39 +0300

    Add authentication middleware to logout route

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 
 import { login, logout, register } from '../controllers/authController'
+import authMiddleware from '../middlewares/authMiddleware'
 
 const router = Router()
 
 router.post('/login', login)
 router.post('/register', register)
-router.post('/logout', logout)
+router.post('/logout', authMiddleware, logout)
 
 export default router
