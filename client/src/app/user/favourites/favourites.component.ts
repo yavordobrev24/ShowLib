c83e9f4aa9af26255f57f6e6fe701e2d50a799b5
Yavor Dobrev <yavordobrev24@gmail.com>
2024-08-10 22:26:08 +0300

    Add required fields and implement constructor in Favourites component

--- client/src/app/user/favourites/favourites.component.ts
+++ client/src/app/user/favourites/favourites.component.ts
@@ -12,4 +12,19 @@ import Favourite from 'src/types/favourite';
   styleUrls: ['./favourites.component.css'],
 })
 export class FavouritesComponent implements OnInit {
+  favouritesForm: FormGroup;
+  selectedType: 'movies' | 'tv-shows' = 'movies';
+  favourites: Favourite[] = [];
+  baseImageUrl: string = 'https://image.tmdb.org/t/p/w500/';
+
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
+  ) {
+    this.favouritesForm = new FormGroup({
+      selectedType: new FormControl(this.selectedType),
+    });
+  }
 }
