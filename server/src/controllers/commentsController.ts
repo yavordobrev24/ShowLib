543b1bf6e93d0ec226ca1d2c0e1e62063ac88fb3
Yavor Dobrev <yavordobrev24@gmail.com>
2024-08-10 10:29:54 +0300

    Add editComment function to commentsController for editing comments

--- server/src/controllers/commentsController.ts
+++ server/src/controllers/commentsController.ts
@@ -6,7 +6,6 @@ const commentRepository = AppDataSource.getRepository(Comment)
 
 export const addComment = async (req: Request, res: Response) => {
   const { content, user_id, user_name, media_id, media_type } = req.body
-  console.log('addComment body ', req.body)
 
   try {
     const newComment = await commentRepository.create({
@@ -17,6 +16,25 @@ export const addComment = async (req: Request, res: Response) => {
       media_type,
     })
     const results = await commentRepository.save(newComment)
+
+    res.json(results)
+  } catch (error) {
+    console.error('Error fetching trending movies:', error)
+    res.status(500).send('Internal Server Error')
+  }
+}
+export const editComment = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const { content, user_name, user_id, media_id, media_type } = req.body
+  try {
+    const results = await commentRepository.update(id, {
+      id: Number(id),
+      content,
+      user_name,
+      user_id,
+      media_id,
+      media_type,
+    })
 
     res.json(results)
   } catch (error) {
