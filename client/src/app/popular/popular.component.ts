9d8d46c24f16d42591c171993b6a4a78b584146a
Yavor Dobrev <yavordobrev24@gmail.com>
2024-08-10 22:28:53 +0300

    Update import in Popular component

--- client/src/app/popular/popular.component.ts
+++ client/src/app/popular/popular.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
-import { Show } from 'src/types/show';
+
+import Show from 'src/types/show';
 
 @Component({
   selector: 'app-popular',
