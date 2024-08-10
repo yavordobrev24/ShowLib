9a01f4dc4f8ba9a887ccf5ae76e1807a213baf86
Yavor Dobrev <yavordobrev24@gmail.com>
2024-08-10 10:17:56 +0300

    Add routes for TV shows with caching middleware

--- server/src/routes/tvShowsRoutes.ts
+++ server/src/routes/tvShowsRoutes.ts
@@ -9,4 +9,8 @@ import cacheMiddleware from '../middlewares/cacheMiddleware'
 import { getTVShowComments } from '../controllers/commentsController'
 
 const router = Router()
+
+router.get('/', cacheMiddleware, getTVShows)
+router.get('/popular', cacheMiddleware, getPopularTVShows)
+router.get('/:id', cacheMiddleware, getTVShowById)
 export default router
