2ea76c9280818d0d92eeea6b0758356d21102483
Yavor Dobrev <yavordobrev24@gmail.com>
2024-08-10 10:22:46 +0300

    Add routes for comments with authentication middleware

--- server/src/routes/commentsRoutes.ts
+++ server/src/routes/commentsRoutes.ts
@@ -8,4 +8,9 @@ import {
 import authMiddleware from '../middlewares/authMiddleware'
 
 const router = Router()
+
+router.post('/', authMiddleware, addComment)
+router.delete('/:id', authMiddleware, deleteComment)
+router.put('/:id', authMiddleware, editComment)
+
 export default router
