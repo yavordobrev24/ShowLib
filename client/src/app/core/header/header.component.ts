19ff08a4552c86c962af32d9d91749d81ff6e743
Yavor Dobrev <yavordobrev24@gmail.com>
2024-08-10 18:58:47 +0300

    Remove unnecessary import and update constructor in Header component

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,24 +1,18 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { ApiService } from 'src/app/api.service';
 import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  
 })
 export class HeaderComponent {
-  searchValue: string | undefined;
-  constructor(
-    private userService: UserService,
-    private apiService: ApiService,
-    private router: Router
-  ) {}
+  constructor(private userService: UserService, private router: Router) {}
   get isLoggedIn(): boolean {
     return this.userService.isLogged;
   }
+
   logout() {
     this.userService.logout();
   }
