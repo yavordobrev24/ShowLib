58a87578b33922482e6037134bee7d63932f4c6f
Yavor Dobrev <yavordobrev24@gmail.com>
2024-08-10 10:22:02 +0300

    Add routes for favourites with authentication middleware

--- server/src/routes/favouritesRoutes.ts
+++ server/src/routes/favouritesRoutes.ts
@@ -7,4 +7,9 @@ import {
 import authMiddleware from '../middlewares/authMiddleware'
 
 const router = Router()
+
+router.get('/', authMiddleware, getFavourite)
+router.post('/', authMiddleware, addFavourite)
+router.delete('/', authMiddleware, deleteFavourite)
+
 export default router
