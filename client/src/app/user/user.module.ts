af0f055dc7825c57bc71d4e55786fdefc2c8e4be
Yavor Dobrev <yavordobrev24@gmail.com>
2024-08-10 11:50:32 +0300

    Update declarations in user module and perform code cleanup

--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
-import { LibraryComponent } from './library/library.component';
+import { FavouritesComponent } from './favourites/favourites.component';
 import { UserRoutingModule } from './user-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -12,12 +12,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  declarations: [
-    RegisterComponent,
-    LoginComponent,
-
-    LibraryComponent,
-  ],
+  declarations: [RegisterComponent, LoginComponent, FavouritesComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
