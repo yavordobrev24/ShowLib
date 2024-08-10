a8bf9255d6b415ce8116c86a0d26e9da42da9090
Yavor Dobrev <yavordobrev24@gmail.com>
2024-08-10 10:32:19 +0300

    Add getTVShowComments function to commentsController for fetching TV show comments

--- server/src/controllers/commentsController.ts
+++ server/src/controllers/commentsController.ts
@@ -68,3 +68,17 @@ export const getMovieComments = async (req: Request, res: Response) => {
     res.status(500).send('Internal Server Error')
   }
 }
+export const getTVShowComments = async (req: Request, res: Response) => {
+  const { id } = req.params
+
+  try {
+    const results = await commentRepository.find({
+      where: [{ media_id: Number(id), media_type: 'tv-shows' }],
+    })
+
+    res.json(results)
+  } catch (error) {
+    console.error('Error fetching trending movies:', error)
+    res.status(500).send('Internal Server Error')
+  }
+}
